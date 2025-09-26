fb596d36177f7fa23af23bc74a85d94af0695bc2
Arne Tarara <arne@datafuse.de>
2025-09-26 19:16:35 +0200

    Added limit and removing open from nginx errors as they show as 404

--- cron/send_nginx_logs.py
+++ cron/send_nginx_logs.py
@@ -13,7 +13,7 @@ from lib import error_helpers
 from lib.job.base import Job
 from lib.global_config import GlobalConfig
 
-def parse_nginx_access_log(log_file):
+def parse_nginx_access_log(log_file, limit=0):
     status_groups = defaultdict(lambda: defaultdict(int))
 
     log_pattern = re.compile(r'\S+ \S+ \S+ \[.*?\] "(.*?)" (\d+)')
@@ -27,15 +27,18 @@ def parse_nginx_access_log(log_file):
             else:
                 raise RuntimeError('Could not match line', line)
 
-    buffer = []
+    buffer = ["Showing all HTTP access with frequency > {limit}"]
     for status, urls in sorted(status_groups.items()):
-        buffer.append(f"\n\nStatus Code {status}")
+
         try:
             http_status = HTTPStatus(int(status)) # can fail bc NGINX has custom HTTP codes like 499
-            buffer.append(f" ({http_status.phrase} - {http_status.description})")
-        except as e:
+            buffer.append(f"\n\nStatus Code {status} ({http_status.phrase} - {http_status.description})")
+        except Exception as e:
+            buffer.append(f"\n\nStatus Code {status}")
             print(e)
         for url, count in sorted(urls.items(), key=lambda x: x[1], reverse=True):
+            if (count < limit):
+                continue
             buffer.append(f"  {url}: {count}")
 
 
@@ -45,22 +48,14 @@ def parse_nginx_access_log(log_file):
 def parse_nginx_error_log(log_file):
     error_groups = defaultdict(int)
 
-    log_pattern = re.compile(r'\S+ \S+ \S+ \S+ \S+ (.*)$')
-
-    with open(log_file, 'r', errors='backslashreplace') as f:
-        for line in f:
-            match = log_pattern.search(line)
-            if match:
-                error = match.groups()
-                error_groups[error] += 1
-            else:
-                raise RuntimeError('Could not match line', line)
-
     buffer = []
     buffer.append(f"Errors:")
 
-    for group, count in sorted(error_groups.items()):
-        buffer.append(f"  {group}: {count}")
+    with open(log_file, 'r', errors='backslashreplace') as f:
+        for line in f:
+            if 'open()' in line and '(2: No such file or directory)' in line:
+                continue
+            buffer.append(line)
 
     return buffer
 
@@ -70,7 +65,7 @@ if __name__ == '__main__':
     try:
         access_log_file = '/var/log/nginx/access.log.1'
         error_log_file = "/var/log/nginx/error.log.1"
-        access_log = parse_nginx_access_log(access_log_file)
+        access_log = parse_nginx_access_log(access_log_file, limit=5)
         error_log = parse_nginx_error_log(error_log_file)
 
         message = '\n'.join(access_log)
