2c66c574394630717fa845919bfea9542989fc80
Arne Tarara <arne@datafuse.de>
2026-04-20 08:35:03 +0200

    Updated send_log_report to show all unfiltered HTTP codes by default; Added 304 restrictions

--- nginx/send_log_report.py
+++ nginx/send_log_report.py
@@ -13,25 +13,6 @@ from lib import error_helpers
 from lib.job.base import Job
 from lib.global_config import GlobalConfig
 
-STATUS_100_EXCLUDES = {
-    'GET /v1/ci/badge/get',
-    'GET / HTTP/1.1',
-    'GET /v2/jobs',
-    'GET /ci.html',
-    'GET /v2/runs',
-    'GET /robots.txt HTTP/1.1',
-    'GET /stats.html',
-    'GET /v1/machines HTTP/1.1',
-    'GET /dist/js/jquery.min.js HTTP/1.1',
-    'GET /dist/js/toast.min.js HTTP/1.1',
-    'GET /js/helpers/main.js HTTP/1.1',
-    'GET /dist/js/transition.min.js HTTP/1.1',
-    'GET /js/helpers/config.js HTTP/1.1',
-    'GET /dist/css/semantic_reduced.min.css HTTP/1.1',
-    'GET /images/favicon.ico HTTP/1.1',
-    'GET /css/green-coding.css HTTP/1.1',
-    'GET /dist/themes/default/assets/fonts/icons.woff2 HTTP/1.1',
-}
 
 STATUS_202_EXCLUDES = {
     'POST /v2/carbondb/add HTTP/1.1',
@@ -43,10 +24,56 @@ STATUS_202_EXCLUDES = {
 STATUS_204_EXCLUDES = {
     'GET /v2/jobs',
     'GET /v1/ci/badge/get',
+    'GET /v1/ci/runs',
+    'GET /v1/ci/measurements',
     'GET /v1/badge/single/[a-z0-9-]{36}',
 }
 
-STATUS_301_EXCLUDES = {'GET / HTTP/1.1'}
+STATUS_301_EXCLUDES = {
+    'GET / HTTP/1.1',
+    'GET /robots.txt HTTP/1.1',
+}
+
+STATUS_304_EXCLUDES = {
+    'GET /v1/ci/badge/get HTTP/2.0',
+    'GET / HTTP/1.1',
+    'GET / HTTP/2.0',
+    'GET /v2/jobs HTTP/2.0',
+    'GET /ci.html HTTP/2.0',
+    'GET /ci.html',
+    'GET /v2/runs  HTTP/2.0',
+    'GET /robots.txt HTTP/2.0',
+    'GET /stats.html HTTP/2.0',
+    'GET /v1/machines HTTP/2.0',
+    'GET /dist/js/jquery.min.js HTTP/2.0',
+    'GET /dist/js/toast.min.js HTTP/2.0',
+    'GET /dist/js/accordion.min.js HTTP/2.0',
+    'GET /dist/css/datatables.min.css HTTP/2.0',
+    'GET /dist/js/datatables.min.js HTTP/2.0',
+    'GET /dist/js/tablesort.min.js HTTP/2.0',
+    'GET /dist/js/tab.min.js HTTP/2.0',
+    'GET /dist/js/echarts.min.js HTTP/2.0',
+    'GET /dist/js/converters.js HTTP/2.0',
+    'GET /js/ci-index.js HTTP/2.0',
+    'GET /dist/js/popup.min.js HTTP/2.0',
+    'GET /dist/js/calendar.min.js HTTP/2.0',
+    'GET /js/ci.js HTTP/2.0',
+    'GET /ci-index.html HTTP/2.0',
+    'GET /runs.html HTTP/2.0',
+    'GET /js/helpers/runs.js HTTP/2.0',
+    'GET /js/index.js HTTP/2.0',
+    'GET /index.html HTTP/2.0',
+    'GET /js/helpers/main.js HTTP/2.0',
+    'GET /js/helpers/converters.js HTTP/2.0',
+    'GET /dist/js/transition.min.js HTTP/2.0',
+    'GET /js/helpers/config.js HTTP/2.0',
+    'GET /dist/css/semantic_reduced.min.css HTTP/2.0',
+    'GET /images/favicon.ico HTTP/2.0',
+    'GET /css/green-coding.css HTTP/2.0',
+    'GET /dist/themes/default/assets/fonts/icons.woff2 HTTP/2.0',
+}
+
+
 STATUS_307_EXCLUDES = {'GET / HTTP/1.1'}
 STATUS_401_EXCLUDES = {'POST /v2/hog/add HTTP/1.1'}
 STATUS_410_EXCLUDES = {'POST /v1/hog/add HTTP/1.1'}
@@ -64,27 +91,22 @@ def _is_interesting_request(status, request, count):
         return request not in STATUS_202_EXCLUDES
     if status_int == 204:
         return not any(re.fullmatch(pattern, request) for pattern in STATUS_204_EXCLUDES)
-    if status_int == 206:
-        return True
     if status_int == 301:
         return request not in STATUS_301_EXCLUDES
     if status_int == 304:
-        return True
+        return request not in STATUS_304_EXCLUDES
     if status_int == 307:
         return request not in STATUS_307_EXCLUDES
-    if status_int == 400:
-        return True
     if status_int == 401:
         return request not in STATUS_401_EXCLUDES
     if status_int == 410:
         return request not in STATUS_410_EXCLUDES
-    if status_int == 404:
-        return True
     if status_int == 422:
         return request not in STATUS_422_EXCLUDES
     if status_int == 444:
         return count > 150
-    return False
+
+    return True
 
 
 def _is_critical_outside_rules(status, _request, _count):
@@ -99,7 +121,7 @@ def _format_status_heading(status):
     return f'Status Code {status}:'
 
 
-def parse_nginx_access_log(log_file, always, limit=0):
+def parse_nginx_access_log(log_file, limit=0):
     status_groups = defaultdict(lambda: defaultdict(int))
 
     log_pattern = re.compile(r'\S+ \S+ \S+ \[.*?\] "(.*?)" (\d+)')
